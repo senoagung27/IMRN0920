4f8613405974b8dade7f551888775623a2074342
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-09-30 16:42:18 +0700

    Update

--- Pekan 1/Tugas3/loopingfor.js
+++ Pekan 1/Tugas3/loopingfor.js
@@ -1,43 +1,18 @@
 console.log("Nomor 1");
 console.log();
 
-function teriak(){
-    return "Halo Sanbers!";
-}
-console.log(teriak());
+var bil = 0;
 
-console.log();
-
-//Soal 2
-
-console.log("Nomor 2");
-console.log();
-
-function kalikan(num1, num2){
-    return num1 * num2;
+console.log("LOOPING PERTAMA");
+while(bil<20){
+    bil+=2;
+    console.log(String(bil), " - I love coding");
 }
 
-var num1 = 12;
-var num2 = 4;
-var hasilKali = kalikan(num1, num2);
-
-console.log(hasilKali);
-
-console.log();
-
-//Soal 3
-
-console.log("Nomor 3");
-console.log();
-
-function introduce(name, age, address, hobby){
-    return "Nama saya " + name + ", umur saya " + String(age) + " tahun, alamat saya di " + address + " dan saya punya hobby yaitu " + hobby + "!";
+console.log("LOOPING KEDUA");
+while(bil>0){
+    console.log(String(bil), " - I will become a mobile developer");
+    bil-=2;
 }
 
-var name = "Fauzan";
-var age = 23;
-var address = "Pondok Gede, Bekasi";
-var hobby = "Tidur";
- 
-var perkenalan = introduce(name, age, address, hobby);
-console.log(perkenalan);
+console.log();
