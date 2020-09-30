a6570f29832eccaeed7f15ced5a00093ba5c6c54
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-09-30 07:33:02 +0700

    clean code

--- Pekan 1/Tugas2/soal1a.js
+++ Pekan 1/Tugas2/soal1a.js
@@ -1,10 +1,10 @@
-var word = 'JavaScript'; 
-var second = 'is'; 
-var third = 'awesome'; 
-var fourth = 'and'; 
-var fifth = 'I'; 
-var sixth = 'love'; 
-var seventh = 'it!';
+var word = "JavaScript";
+var second = "is";
+var third = "awesome";
+var fourth = "and";
+var fifth = "I";
+var sixth = "love";
+var seventh = "it!";
 console.log(word, second, third, fourth, fifth, sixth, seventh);
 // console.log(word);
 // console.log(second);
