d90baa0d3b40d56c9cc9d9c764f3abe68be22c49
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-10-01 21:36:34 +0700

    Update Data

--- Pekan 1/Tugas4/soal2.js
+++ Pekan 1/Tugas4/soal2.js
@@ -2,8 +2,8 @@ function kalikan(a, b) {
   return a * b;
 }
 
-var num1 = 12;
-var num2 = 4;
-
-var hasil = kalikan(num1, num2);
-console.log(hasil);
+var num1 = 12
+var num2 = 4
+ 
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali) // 48
