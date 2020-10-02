fadb6679ba6fca5bc24f27f51992ae1f7e2cc71c
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-10-02 09:16:14 +0700

    Conflict Fix

--- Pekan 1/Tugas4/soal1.js
+++ Pekan 1/Tugas4/soal1.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 function teriak(){
     return "Halo Sanbers!";
 }
 console.log(teriak());
 
 console.log();
-=======
-function teriak() {
-  console.log("Halo Sanbers!");
-}
-teriak();
->>>>>>> cd6219854f1283a961c91e8d1f0944e77e51fa0b
