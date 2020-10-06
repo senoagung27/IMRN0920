6b0559b636cc4a3a7a5c6bdcd2915617184d4f9c
Seno Agung Pradityo <senoagung27@yahoo.com>
2020-10-07 06:38:32 +0700

    Update

---
+++ Pekan 2/Tugas7/soal1.js
@@ -0,0 +1,10 @@
+console.log("Nomor 1");
+console.log();
+
+const golden = () => {
+    console.log("this is golden!!")
+}
+   
+golden();
+
+console.log();
---
+++ Pekan 2/Tugas7/soal3.js
@@ -0,0 +1,16 @@
+console.log("Nomor 3");
+console.log();
+
+const newObject = {
+    firstName: "Harry",
+    lastName: "Potter Holt",
+    destination: "Hogwarts React Conf",
+    occupation: "Deve-wizard Avocado",
+    spell: "Vimulus Renderus!!!"
+}
+
+const {firstName, lastName, destination, occupation} = newObject;
+
+console.log(firstName, lastName, destination, occupation)
+
+console.log();
